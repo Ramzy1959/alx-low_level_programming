0d72b39d1fb8f8347af009fe06ffeec495e12a6b
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-17 05:17:12 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return: 1 (success)
+ * main - A program that  prints exactly " and that piece of art is useful\" -
+ *  Dora Korpar, 2015-10-19, followed by a new line, to the standard error."
+ *
+ *  Return: (1)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
