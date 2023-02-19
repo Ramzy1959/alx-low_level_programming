3e9ffa34043e9c8ef8849b2c1941baf1d329d08d
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-19 04:48:36 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z'; c++)
+	{
+	if (c != 'p' && c != 'e')
+	{
+	putchar(c);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
