1def41b207543477966fb567c0819d551365f23d
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-19 08:35:45 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: prints numbers of base 16 in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	
+	char n;
+
+	for (i = 0; i < 16; i++)
+	{
+	if (n < 10)
+	{
+	  n = i + '0';
+	} else
+	{
+	  = i - 10 + 'a';
+	}
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
