f6bd2090b3680b6667d8130cd0b2d2859a412405
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-19 01:38:34 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'A program that alphabet in lower case'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char 'c';
+
+	for (c = 'a'; c <= 'z', ++c)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
