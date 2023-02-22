ddfd21d6577c8f8dcda0af9184959931d791ae74
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-22 12:15:23 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - printing jack's minutes
+ * Return: success
+ */
+void jack_bauer(void)
+{
+	int hr, minute;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
