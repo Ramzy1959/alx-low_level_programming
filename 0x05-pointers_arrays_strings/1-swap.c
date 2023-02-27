bc821573726bbaa85e469b19b871b983a9fe26ad
Ramzy1959 <ramzyoriol@yahoo.com>
2023-02-27 13:38:14 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap twi integers
+ * @a: value 1 to be swapped
+ * @b: value 2 to be swapped
+ * Return: Always 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+
+	*a = *b;
+	*b = swap;
+}
